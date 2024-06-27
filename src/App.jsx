98bf6be2b7b2ef47c5c5a83a4102ba73278459c6
henrytrust1111 <henrytrust1111@gmail.com>
2024-06-27 09:30:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <p className='text-xl font-bold text-red-500 qodef-icon-font-elegant icon_link qodef-icon-element'>fdgf </p>
+    </>
+  )
+}
+
+export default App
