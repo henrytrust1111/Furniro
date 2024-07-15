cb55bf2386705dde3ed65a3ee697e23306454972
henrytrust1111 <henrytrust1111@gmail.com>
2024-07-15 07:14:50 +0100

    LoginService22231

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import logo from '/icons/logo.svg'
 
 const Header = () => {
   return (
     <header className="bg-white shadow">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold">Furniro</div>
+      <div className="w-11/12 py-4 flex justify-between items-center">
+        <div className="text-3xl space-x-1 font-bold flex">
+          <img src={logo} alt="" />
+          <h3>Furniro</h3>
+        </div>
         <nav className="space-x-4">
           <a href="/" className="text-gray-600 hover:text-gray-900">Home</a>
           <a href="/shop" className="text-gray-600 hover:text-gray-900">Shop</a>
