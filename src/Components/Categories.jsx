6bfecc0aeb0c62617237714a1a560acc29783ad4
henrytrust1111 <henrytrust1111@gmail.com>
2024-07-16 04:25:48 +0100

    Working on the Logo1

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import dining from '/images/dining.png'
+import living from '/images/living.png'
+import bedroom from '/images/bedroom.png'
 
 const Categories = () => {
+  const data = [
+    {id: 1, image: dining, des:"Dining"},
+    {id: 2, image: living, des:"Living"},
+    {id: 3, image: bedroom, des:"Bedroom"},
+  ]
   return (
-    <section className="py-16">
+    <section className="py-16 font-[poppins]">
       <div className="container mx-auto px-4 text-center">
-        <h2 className="text-2xl font-bold mb-8">Browse The Range</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {['Dining', 'Living', 'Bedroom'].map((category) => (
-            <div key={category} className="bg-gray-100 p-8 rounded-lg">
-              <img src={`/${category.toLowerCase()}.jpg`} alt={category} className="mb-4 w-full h-48 object-cover rounded-lg"/>
-              <h3 className="text-xl font-bold">{category}</h3>
+        <h2 className="text-2xl font-bold ">Browse The Range</h2>
+        <p className='mb-8 px-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-8">
+          {data.map((e) => (
+           <div key={e.id}>
+            <div  className="rounded-lg">
+              <img src={e.image} alt={e.des} className="mb-4 w-full h-full object-cover rounded-lg"/>
             </div>
+              <h3 className="text-xl font-bold">{e.des}</h3>
+           </div>
           ))}
         </div>
       </div>
