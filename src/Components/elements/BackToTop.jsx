ea362664b31daab34f0740bccd15d16bf20ab9a5
henrytrust1111 <henrytrust1111@gmail.com>
2024-08-06 12:29:32 +0100

    done with first section4

--- src/Components/elements/BackToTop.jsx
+++ src/Components/elements/BackToTop.jsx
@@ -1,24 +1,76 @@
-import ScrollToTop from "react-scroll-up";
+// import ScrollToTop from "react-scroll-up";
+
+// export default function BackToTop() {
+//   return (
+//     <ScrollToTop showUnder={160}>
+//       <a id="scrollUp" >
+//         <svg
+//           className="w-4 h-4"
+//           fill="none"
+//           stroke="currentColor"
+//           viewBox="0 0 24 24"
+//           xmlns="http://www.w3.org/2000/svg"
+//         >
+//           <path
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//             strokeWidth="2"
+//             d="M5 10l7-7m0 0l7 7m-7-7v18"
+//           ></path>
+//         </svg>
+//       </a>
+//     </ScrollToTop>
+//   );
+// }
+
+
+import { useEffect, useState } from "react";
 
 export default function BackToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled down
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Scroll to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
-    <ScrollToTop showUnder={160}>
-      <a id="scrollUp">
-        <svg
-          className="w-4 h-4"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 10l7-7m0 0l7 7m-7-7v18"
-          ></path>
-        </svg>
-      </a>
-    </ScrollToTop>
+    <div className="back-to-top ">
+      {isVisible && (
+        <div onClick={scrollToTop} className="fixed bottom-4 right-4 cursor-pointer -bg--clr-primary text-white p-2 rounded-full z-50">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 10l7-7m0 0l7 7m-7-7v18"
+            ></path>
+          </svg>
+        </div>
+      )}
+    </div>
   );
 }
