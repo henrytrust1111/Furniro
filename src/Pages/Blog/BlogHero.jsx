89732cf2bfd8b00f687a149748d38f9f6a4345c1
henrytrust1111 <henrytrust1111@gmail.com>
2024-08-07 09:44:47 +0100

    Created a reusuable Hero Component

--- src/Pages/Blog/BlogHero.jsx
+++ src/Pages/Blog/BlogHero.jsx
@@ -1,36 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import logo from "/icons/logo.svg";
-import { PiCaretRightBold } from "react-icons/pi";
-import blogHero from "/images/blog.png";
+import ReuseableHero from "../../Components/ReuseableHero";
 
 const BlogHero = () => {
-  const nav = useNavigate();
-  const handleHome = () => {
-    nav("/");
-  };
+
   return (
-    <div className="relative w-screen mt-[53px] font-[poppins]">
-      <img src={blogHero} className="w-full " alt="" onClick={handleHome} />
-      <div className="absolute w-full h-full top-0 flex flex-col items-center justify-center cursor-pointer md:gap-1 lg:gap-3">
-        <img src={logo} alt="" className="w-8 md:!w-14" />
-        <p className="font-medium text-2xl lg:text-5xl">Blog</p>
-        <div className="flex items-center -text--clr-secondary">
-          <span
-            className="text-black font-[poppins] font-medium text-sm md:text-base"
-            onClick={handleHome}
-          >
-            Home{" "}
-          </span>{" "}
-          <span className="text-black text-sm md:text-base">
-            <PiCaretRightBold />
-          </span>
-          <span className="text-black font-[poppins] font-light text-sm md:text-base">
-            Blog
-          </span>
-        </div>
-      </div>
-    </div>
+    <>
+    <ReuseableHero page={"Blog"} />
+    </>
   );
 };
 
