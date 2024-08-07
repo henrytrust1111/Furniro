09b85f5893cf789c4ce337c55841a9a2da209e60
Adoyi Rose <rozarioehiz@gmail.com>
2024-08-07 17:43:09 +0100

    media screen for shop

---
+++ src/Pages/Shop/ShopPage.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import './ShopPage.css'
+
+
+const ShopPage = () =>{
+    return(
+
+           <div className="page-buttons">
+         <button className="num-button">1</button>
+         <button className="num-button">2</button>
+         <button className="num-button">3</button>
+         <button className="next-button">Next</button>
+       </div>
+    )
+};
+export default ShopPage
