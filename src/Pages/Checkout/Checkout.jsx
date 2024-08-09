53bd3636239c1423d78f79c44067cc22fa2d0644
henrytrust1111 <henrytrust1111@gmail.com>
2024-08-09 06:23:52 +0100

    fixing the shopHero cpmponent4

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ScrollToTop from "../../Containers/ScrollToTop";
 import ReuseableHero from "../../Components/ReuseableHero";
 
-const Cart = () => {
+const Checkout = () => {
   return (
     <>
       <ScrollToTop />
-      <ReuseableHero page={"Cart"} page1={"Cart"} />
+      <ReuseableHero page={"Checkout"} page1={"Checkout"} />
     </>
   );
 };
 
-export default Cart;
+export default Checkout;
