e2fef574e351087087ea019b35b1774bbfc53ead
Elo-oghene Olokor <Olokorelo52@gmail.com>
2024-08-26 16:21:54 +0100

    cart

--- src/Pages/Cart/MainCart.jsx
+++ src/Pages/Cart/MainCart.jsx
@@ -2,9 +2,27 @@ import React from "react";
 const Maincart = () => {
     return (
       <>
-        <div className="mb-14 mt-14">
-            <div className="w-full px-32 flex gap-14">
-                <div className="w-[60%] flex flex-col gap-6">
+        <div className="w-full mb-14 mt-14">
+            <div className="w-full lg:!px-32 px-8 flex flex-col lg:flex-row gap-14">
+                <div className="flex flex-col gap-6 lg:hidden">
+                    <div className="flex items-center gap-2">
+                        <div>Product:</div>
+                        <p className="text-[#9F9F9F] text-sm">Asgard Sofa</p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <div>Price:</div>
+                        <p className="text-[#9F9F9F] text-sm">Rs.250,000.00</p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <div>Quantity:</div>
+                        <p className="w-6 h-6 rounded-sm border-[0.5px] flex items-center justify-center text-sm border-[#9F9F9F] text-[#000000]">1</p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <div>Subtotal:</div>
+                        <p className="text-[#000000] text-sm">Rs.250,000.00</p>
+                    </div>
+                </div>
+                <div className=" lg:w-[60%] hidden lg:flex flex-col gap-6">
                     <div className="w-full h-14 bg-[#F9F1E7] flex justify-center gap-14 text-sm items-center ">
                         <p>Product</p>
                         <p>Price</p>
@@ -26,7 +44,7 @@ const Maincart = () => {
                         <p><img src="/ant-design_delete-filled.png" alt="" /></p>
                     </div>
                 </div>
-                <div className="w-[23%] h-60 bg-[#F9F1E7] flex flex-col items-center gap-8 py-4 ">
+                <div className="lg:w-[23%] h-60 bg-[#F9F1E7] flex flex-col items-center gap-8 py-4 ">
                     <p className="text-[#000000]">Cart Totals</p>
                     <div className="flex items-center gap-6">
                         <p className="text-sm">Subtotal</p>
