50b7ed9071fe7db92f9aba5b30cf109414c1f821
henrytrust1111 <henrytrust1111@gmail.com>
2024-11-22 08:04:15 +0100

    setting up redux1

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,21 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from './Global/Store.jsx'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 const queryClient = new QueryClient();
-
+let myPersistorVariable = persistStore(store)
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={myPersistorVariable}>
       <App />
+      </PersistGate>
+    </Provider>
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>
