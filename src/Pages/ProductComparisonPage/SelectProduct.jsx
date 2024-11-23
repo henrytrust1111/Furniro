7296b41995e951bf144c828c874cf87630566d92
henrytrust1111 <henrytrust1111@gmail.com>
2024-11-23 17:34:58 +0100

    setting up aos2

--- src/Pages/ProductComparisonPage/SelectProduct.jsx
+++ src/Pages/ProductComparisonPage/SelectProduct.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const SelectProduct = ({ drowDownList }) => {
   const [data, setData] = useState(drowDownList);
 
+  useEffect(() => {
+    AOS.init({
+      duration: 800, 
+      once: true, 
+    });
+  }, []);
   return (
     <div className="absolute -text--clr-primary bg-black w-full left-0">
       {data?.map((e) => (
