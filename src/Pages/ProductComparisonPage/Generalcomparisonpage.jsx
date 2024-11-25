b00fa40f41da679ba12a14838a98443c3f1b5323
henrytrust1111 <henrytrust1111@gmail.com>
2024-11-25 09:27:53 +0100

    comparison Page12

--- src/Pages/ProductComparisonPage/Generalcomparisonpage.jsx
+++ src/Pages/ProductComparisonPage/Generalcomparisonpage.jsx
@@ -1,62 +1,59 @@
 import "./Generalcomparisonpage.css";
 
-const General =()=>{
-    return(
-  <>
-  <div className="Genralcomparisonpage">
-    <div className="Genralcomparisonpagewrapper">
-        <div className="GeneralCompari">
+const General = () => {
+  return (
+    <>
+      <div className="Genralcomparisonpage">
+        <div className="Genralcomparisonpagewrapper">
+          <div className="GeneralCompari">
             <div className="GeneralCompariBoxup">
-                <h3>General</h3>
+              <h3>General</h3>
             </div>
             <div className="GeneralCompariBoxdown">
-            <div className="GeneralCompariBoxdownwrap1">
+              <div className="GeneralCompariBoxdownwrap1">
                 <p>Sales Package</p>
                 <p>Model Number</p>
                 <p>Secondary Materials</p>
                 <p>Configuration</p>
                 <p>Uphasltery Material</p>
                 <p>Uphasltery Color</p>
-                </div>
+              </div>
             </div>
-        </div>
-        <div className="GeneralCompari">
-            <div className="GeneralCompariBoxup1">
-            </div>
+          </div>
+          <div className="GeneralCompari">
+            <div className="GeneralCompariBoxup1"></div>
             <div className="GeneralCompariBoxdown">
-                <div className="GeneralCompariBoxdownwrap">
+              <div className="GeneralCompariBoxdownwrap">
                 <p>Sales Package</p>
                 <p>Model Number</p>
                 <p>Secondary Materials</p>
                 <p>Configuration</p>
                 <p>Uphasltery Material</p>
                 <p>Uphasltery Color</p>
-                </div>
+              </div>
             </div>
-        </div>
-        <div className="GeneralCompari">
-            <div className="GeneralCompariBoxup1">
-            </div>
+          </div>
+          <div className="GeneralCompari">
+            <div className="GeneralCompariBoxup1"></div>
             <div className="GeneralCompariBoxdown">
-            <div className="GeneralCompariBoxdownwrap">
+              <div className="GeneralCompariBoxdownwrap">
                 <p>Sales Package</p>
                 <p>Model Number</p>
                 <p>Secondary Materials</p>
                 <p>Configuration</p>
                 <p>Uphasltery Material</p>
                 <p>Uphasltery Color</p>
-                </div>
+              </div>
             </div>
-        </div>
-        <div className="GeneralCompari1">
-            <div className="GeneralCompariBoxup">
-            </div>
+          </div>
+          <div className="GeneralCompari1">
+            <div className="GeneralCompariBoxup"></div>
             <div className="GeneralCompariBoxdown"></div>
+          </div>
         </div>
-    </div>
-  </div>
-  </>      
- )
-}
+      </div>
+    </>
+  );
+};
 
-export default General
+export default General;
