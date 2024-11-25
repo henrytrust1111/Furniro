20e1a91e8218d5d3fa47bb0ba244ec59021e0209
henrytrust1111 <henrytrust1111@gmail.com>
2024-11-25 09:37:55 +0100

    comparison Page13

--- src/Pages/ProductComparisonPage/Generalcomparisonpage.jsx
+++ src/Pages/ProductComparisonPage/Generalcomparisonpage.jsx
@@ -1,57 +1,7 @@
-import "./Generalcomparisonpage.css";
-
 const General = () => {
   return (
     <>
-      <div className="Genralcomparisonpage">
-        <div className="Genralcomparisonpagewrapper">
-          <div className="GeneralCompari">
-            <div className="GeneralCompariBoxup">
-              <h3>General</h3>
-            </div>
-            <div className="GeneralCompariBoxdown">
-              <div className="GeneralCompariBoxdownwrap1">
-                <p>Sales Package</p>
-                <p>Model Number</p>
-                <p>Secondary Materials</p>
-                <p>Configuration</p>
-                <p>Uphasltery Material</p>
-                <p>Uphasltery Color</p>
-              </div>
-            </div>
-          </div>
-          <div className="GeneralCompari">
-            <div className="GeneralCompariBoxup1"></div>
-            <div className="GeneralCompariBoxdown">
-              <div className="GeneralCompariBoxdownwrap">
-                <p>Sales Package</p>
-                <p>Model Number</p>
-                <p>Secondary Materials</p>
-                <p>Configuration</p>
-                <p>Uphasltery Material</p>
-                <p>Uphasltery Color</p>
-              </div>
-            </div>
-          </div>
-          <div className="GeneralCompari">
-            <div className="GeneralCompariBoxup1"></div>
-            <div className="GeneralCompariBoxdown">
-              <div className="GeneralCompariBoxdownwrap">
-                <p>Sales Package</p>
-                <p>Model Number</p>
-                <p>Secondary Materials</p>
-                <p>Configuration</p>
-                <p>Uphasltery Material</p>
-                <p>Uphasltery Color</p>
-              </div>
-            </div>
-          </div>
-          <div className="GeneralCompari1">
-            <div className="GeneralCompariBoxup"></div>
-            <div className="GeneralCompariBoxdown"></div>
-          </div>
-        </div>
-      </div>
+      <div className="grid grid-cols-3 bg-green-600 h-32"></div>
     </>
   );
 };
