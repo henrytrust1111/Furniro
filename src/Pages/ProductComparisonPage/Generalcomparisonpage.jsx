b4fc4e0e8875e995c16b1e241efc04e021c49554
henrytrust1111 <henrytrust1111@gmail.com>
2024-11-26 09:17:28 +0100

    comparisonPage1.4

--- src/Pages/ProductComparisonPage/Generalcomparisonpage.jsx
+++ src/Pages/ProductComparisonPage/Generalcomparisonpage.jsx
@@ -7,33 +7,33 @@ const General = () => {
         <div className="grid grid-rows-7 gap-4 border-r-2">
          <p className="row-start-1 col-start-1 row-end-1 col-end-1 font-medium text-2xl"> General</p>
          <p className="col-start-1 col-end-1 row-start-2 row-end-2 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Sales Package</p>
-         <p className="col-start-1 col-end-1 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-20">Model Number</p>
-         <p className="col-start-1 col-end-1 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-20">Secondary Materials</p>
-         <p className="col-start-1 col-end-1 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-20">Configuration</p>
-         <p className="col-start-1 col-end-1 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-20">Upholstery Material</p>
-         <p className="col-start-1 col-end-1 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-20">Upholstery Color</p>
+         <p className="col-start-1 col-end-1 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Model Number</p>
+         <p className="col-start-1 col-end-1 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Secondary Materials</p>
+         <p className="col-start-1 col-end-1 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Configuration</p>
+         <p className="col-start-1 col-end-1 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Upholstery Material</p>
+         <p className="col-start-1 col-end-1 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Upholstery Color</p>
         </div>
 
 
                    {/* col 2 */}
        <div className="grid grid-rows-7 border-r-2">
-        <p className="col-start-2 col-end-2 row-start-2 row-end-2 text-xs md:text-sm lg:text-base">1 sectional sofa</p>
-        <p className="col-start-2 col-end-2 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-20">TFCBLIGRBL6SRHS</p>
-        <p className="col-start-2 col-end-2 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-20">Solid Wood</p>
-        <p className="col-start-2 col-end-2 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-20">L-shaped</p>
-        <p className="col-start-2 col-end-2 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-20">Fabric + Cotton</p>
-        <p className="col-start-2 col-end-2 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-20">Bright Grey & Lion</p>
+        <p className="col-start-2 col-end-2 row-start-2 row-end-2 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">1 sectional sofa</p>
+        <p className="col-start-2 col-end-2 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">TFCBLIGRBL6SRHS</p>
+        <p className="col-start-2 col-end-2 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Solid Wood</p>
+        <p className="col-start-2 col-end-2 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">L-shaped</p>
+        <p className="col-start-2 col-end-2 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Fabric + Cotton</p>
+        <p className="col-start-2 col-end-2 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Bright Grey & Lion</p>
        </div>
 
         {/* col 3 */}
 
         <div className="grid grid-rows-7 border-r-2">
-        <p className="col-start-3 col-end-3 row-start-2 row-end-2 text-xs md:text-sm lg:text-base break-words w-20">1 Three Seater, 2 Single Seater</p>
-        <p className="col-start-3 col-end-3 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-20">DTUBLIGRBL568</p>
-        <p className="col-start-3 col-end-3 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-20">Solid Wood</p>
-        <p className="col-start-3 col-end-3 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-20">L-shaped</p>
-        <p className="col-start-3 col-end-3 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-20">Fabric + Cotton</p>
-        <p className="col-start-3 col-end-3 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-20">Bright Grey & Lion</p>
+        <p className="col-start-3 col-end-3 row-start-2 row-end-2 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">1 Three Seater, 2 Single Seater</p>
+        <p className="col-start-3 col-end-3 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">DTUBLIGRBL568</p>
+        <p className="col-start-3 col-end-3 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Solid Wood</p>
+        <p className="col-start-3 col-end-3 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">L-shaped</p>
+        <p className="col-start-3 col-end-3 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Fabric + Cotton</p>
+        <p className="col-start-3 col-end-3 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-20 sm:w-full">Bright Grey & Lion</p>
         </div>
 
         {/* col 4 */}
