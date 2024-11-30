003cfcc33d93e347d2a81c8c0d5f0c04232c219d
henrytrust1111 <henrytrust1111@gmail.com>
2024-11-30 01:43:21 +0100

    comparisonPage4.5

--- src/Pages/ProductComparisonPage/Warrantycomparison.jsx
+++ src/Pages/ProductComparisonPage/Warrantycomparison.jsx
@@ -19,7 +19,7 @@ const Warrantycomparison = () => {
           <p className="col-start-2 col-end-2 row-start-2 row-end-2 text-xs md:text-sm lg:text-base break-words w-full flex items-center"></p>
           <p className="col-start-2 col-end-2 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-full flex items-center">1 Year Manufacturing Warranty</p>
           <p className="col-start-2 col-end-2 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-full flex items-center">For Warranty Claims or Any Product Related Issues Please Email at furniture.com</p>
-          <p className="col-start-2 col-end-2 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-full flex items-center">Warranty Against Manufacturing Defect</p>
+          <div className="col-start-2 col-end-2 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-full flex items-center">Warranty Against Manufacturing Defect</div>
           <p className="col-start-2 col-end-2 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-full flex items-center">The Warranty Does Not Cover Damages Due To Usage Of The Product Beyond Its Intended Use And Wear & Tear In The Natural Course Of Product Usage.</p>
           <div className="col-start-2 col-end-2 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-full  flex items-center">1 Year</div>
           <p className="col-start-2 col-end-2 row-start-8 row-end-8 text-xs md:text-sm lg:text-base break-words w-full  flex items-center"> <button className="-bg--clr-primary text-white h-11 px-4 hidden">Add To Cart</button> </p>
@@ -30,10 +30,9 @@ const Warrantycomparison = () => {
           <div className="grid grid-rows-7 border-r-2 gap-2 w-full hyphenated">
           <p className="col-start-3 col-end-3 row-start-2 row-end-2 text-xs md:text-sm lg:text-base break-words w-full flex items-center"></p>
           <p className="col-start-3 col-end-3 row-start-3 row-end-3 text-xs md:text-sm lg:text-base break-words w-full flex items-center">1.2 Year Manufacturing Warranty</p>
-          <p className="col-start-3 col-end-3 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-full  flex items-center ">For Warranty Claims or Any Product Related Issues Please Email at supp</p>
+          <p className="col-start-3 col-end-3 row-start-4 row-end-4 text-xs md:text-sm lg:text-base break-words w-full flex items-center ">For Warranty Claims or Any Product Related Issues Please Email at supp</p>
           <p className="col-start-3 col-end-3 row-start-5 row-end-5 text-xs md:text-sm lg:text-base break-words w-full flex items-center">Warranty of the product is limited to manufacturing defects only.</p>
-          <p className="col-start-3 col-end-3 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-full flex items-center">The Warranty Does Not Cover Damages Due To Usage Of The Product Beyond Its Intended Use And Wear & Tear In The Natural Course Of Product Usage
-            .</p>
+          <p className="col-start-3 col-end-3 row-start-6 row-end-6 text-xs md:text-sm lg:text-base break-words w-full flex items-center">The Warranty Does Not Cover Damages Due To Usage Of The Product Beyond Its Intended Use And Wear & Tear In The Natural Course Of Product Usage.</p>
           <p className="col-start-3 col-end-3 row-start-7 row-end-7 text-xs md:text-sm lg:text-base break-words w-full flex items-center">3 Months</p>
           <p className="col-start-3 col-end-3 row-start-8 row-end-8 text-xs md:text-sm lg:text-base break-words w-full flex items-center"><button className="-bg--clr-primary text-white h-11 px-4 hidden">Add To Cart</button></p>
           </div>
