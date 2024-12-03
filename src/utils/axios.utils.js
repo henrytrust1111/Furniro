25dc08edb47197845e55d3575e3bc9ed719a5347
henrytrust1111 <henrytrust1111@gmail.com>
2024-12-03 06:52:55 +0100

    addToCart2.2

--- src/utils/axios.utils.js
+++ src/utils/axios.utils.js
@@ -1,17 +1,17 @@
 // Axios Interceptor
 import axios from "axios";
 
-const client = axios.create({ baseURL: "https://funiro-furnitures.onrender.com" });
+const client = axios.create({
+  baseURL: "https://funiro-furnitures.onrender.com",
+});
 
 export const request = ({ ...options }) => {
   const token = localStorage.getItem("token");
-   console.log(token);
-   
   client.defaults.headers.common.Authorization = `Bearer ${token}`;
   const onSuccess = (response) => response;
   const onError = (error) => {
     // Additional catch errors and additional logging in here
     return error;
   };
-  return client(options).then(onSuccess).catch(onError)
+  return client(options).then(onSuccess).catch(onError);
 };
