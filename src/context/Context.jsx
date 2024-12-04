35b1c319e5369c563c86c0de947d07a925ca0ac4
henrytrust1111 <henrytrust1111@gmail.com>
2024-12-04 10:22:28 +0100

    viewcart3.2

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -25,7 +25,7 @@ const Context = ({ children }) => {
   const onError = (error) => {
     console.log("An Error occurred", error);
   };
-  const { isLoading, data, isError, error, isFetching, refetch } =
+  const { isLoading, data, error, refetch } =
     UseQueryCustomHook(onError, onSuccess);
 
   const { isLoading: isLoadingCart, data: cart } = useViewCart(
