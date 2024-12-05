2498f673de455b27cbc0ddb695b8cce893f3c768
henrytrust1111 <henrytrust1111@gmail.com>
2024-12-05 03:02:39 +0100

    formatting1.7

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -50,7 +50,6 @@ const Products = ({ Title }) => {
   };
 
   if (isLoading && !data) {
-    
     return (
       <section className="py-16 font-[poppins]">
         <div className="container mx-auto px-4 text-center -text--clr-primary flex items-center justify-center">
